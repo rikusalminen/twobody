d77e4d798c5c582360b55a92d5b3936f12164528
Riku Salminen <riku.salminen@tkk.fi>
2015-06-13 22:47:28 +0300

    gauss: gauss_test eccentric anomaly limits
    [skip ci]

--- test/twobody/gauss_test.c
+++ test/twobody/gauss_test.c
@@ -22,24 +22,24 @@ void gauss_test(
     double n = conic_mean_motion(mu, p, e);
     double t0 = 0.0;
 
-    double E1 = (-1.0 + params[3] * 2.0) * M_PI;
+    double E1 = (-1.0 + params[3] * 1.5) * M_PI;
     double f1 = anomaly_eccentric_to_true(e, E1);
     double M1 = anomaly_eccentric_to_mean(e, E1);
     double t1 = t0 + M1 / n;
     double r1 = eccentric_radius(p, e, E1);
 
-    double E2 = (-1.0 + params[4] * 2.0) * M_PI;
+    double E2 = e < 1.0 ?
+        E1 + (0.1 + params[4] * 1.9) * M_PI :
+        E1 + (M_PI - E1) * (0.1 + params[4] * 0.9);
     double f2 = anomaly_eccentric_to_true(e, E2);
     double M2 = anomaly_eccentric_to_mean(e, E2);
     double t2 = t0 + M2 / n;
     double r2 = eccentric_radius(p, e, E2);
 
-    if(ZEROF(e - 1.0) || E2 <= E1) // XXX: change inputs
-        return;
-
     double z0 = f2-f1;
     double z = gauss_iterate_z(mu, r1, r2, f2-f1, t2-t1, z0, 30);
 
+    ASSERT(isfinite(z), "z not NaN");
     ASSERT_EQF(z, (E2-E1)*(E2-E1),
         "z = dE^2");
 }
