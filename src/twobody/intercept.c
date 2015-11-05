70b9c775db68f1f098c8c6476eefa4b85d0a0fee
Riku Salminen <riku.salminen@tkk.fi>
2015-11-05 13:48:33 +0200

    intercept: clean up debugging code

--- src/twobody/intercept.c
+++ src/twobody/intercept.c
@@ -256,57 +256,6 @@ int intercept_times(
     return num_times;
 }
 
-#include <stdio.h> // XXX: kill me ! 
-
-int intercept_dump(
-    const struct orbit *orbit1,
-    const struct orbit *orbit2,
-    double t0, double t1) {
-
-    double mu = orbit_gravity_parameter(orbit1);
-
-    double p[2] = {
-        orbit_semi_latus_rectum(orbit1),
-        orbit_semi_latus_rectum(orbit2)
-    };
-    double e[2] = {
-        orbit_eccentricity(orbit1),
-        orbit_eccentricity(orbit2)
-    };
-    double n[2] = {
-        conic_mean_motion(mu, p[0], e[0]),
-        conic_mean_motion(mu, p[1], e[1])
-    };
-    double t_pe[2] = {
-        orbit_periapsis_time(orbit1),
-        orbit_periapsis_time(orbit2),
-    };
-
-    FILE *file = fopen("intercept_distance.txt", "w");
-
-    int max_steps = 1000;
-    for(int i = 0; i < max_steps; ++i) {
-        double t = t0 + (i / (double)(max_steps-1)) * (t1-t0);
-
-        vec4d pos[2], vel[2];
-        for(int o = 0; o < 2; ++o) {
-            const struct orbit *orbit = o ? orbit2 : orbit1;
-            double M = (t - t_pe[o]) * n[o];
-            double E = anomaly_mean_to_eccentric(e[o], M);
-            pos[o] = orbit_position_eccentric(orbit, E);
-            vel[o] = orbit_velocity_eccentric(orbit, E);
-        }
-
-        double dist = mag(pos[1]-pos[0]);
-        double vrel = dot(vel[1]-vel[0], pos[1]-pos[0])/dist;
-
-        fprintf(file, "%lf\t%lf\t%lf\n", t, dist, vrel);
-    }
-
-    fclose(file);
-    return 0;
-}
-
 double intercept_search(
     const struct orbit *orbit1,
     const struct orbit *orbit2,
@@ -315,11 +264,6 @@ double intercept_search(
     double target_distance,
     int max_steps,
     struct intercept *intercept) {
-
-//#define INTERCEPT_DEBUG
-#ifdef INTERCEPT_DEBUG
-    intercept_dump(orbit1, orbit2, t0, t1);
-#endif
 
     double mu = orbit_gravity_parameter(orbit1);
 
@@ -348,9 +292,6 @@ double intercept_search(
         mu/conic_periapsis(p[0], e[0]) +
         mu/conic_periapsis(p[1], e[1]);
 
-    //vec4d nodes = cross(orbit1->normal_axis, orbit2->normal_axis);
-    //int coplanar = dot(nodes, nodes) < DBL_EPSILON;
-
     vec4d pos[2] = {{ NAN, NAN, NAN, NAN}, { NAN, NAN, NAN, NAN}};
     vec4d vel[2] = {{ NAN, NAN, NAN, NAN}, { NAN, NAN, NAN, NAN}};
     vec4d dr = { NAN, NAN, NAN, NAN}, dv = { NAN, NAN, NAN, NAN};
@@ -362,22 +303,11 @@ double intercept_search(
 
     double min_dt = (t1-t0) / (max_steps/2);
 
-#ifdef INTERCEPT_DEBUG
-    FILE *file = fopen("intercept_steps.txt", "w");
-#endif
-
     double t = t0, prev_time = NAN, t_end = t0;
     int prev_sgn = 0;
     for(int step = 0; step < max_steps; ++step) {
         for(int o = 0; o < 2; ++o) {
             double M = (t - t_pe[o]) * n[o];
-// XXX: enable this
-#if 0
-            if(conic_parabolic(e[o]))
-                E[o] = anomaly_mean_to_eccentric(e[o], M);
-            else
-                E[o] = anomaly_eccentric_iterate(e[o], M, E[o], -1);
-#endif
             E[o] = anomaly_mean_to_eccentric(e[o], M);
 
             pos[o] = orbit_position_eccentric(orbits[o], E[o]);
@@ -390,38 +320,20 @@ double intercept_search(
         vrel = dot(dr, dv) / dist;
         int sgn = sign(vrel) * sign(dist - target_distance);
 
-#ifdef INTERCEPT_DEBUG
-        fprintf(file, "%d\t%lf\t%lf\t%lf\n", step, t, dist, vrel);
-#endif
-
         double dt = min_dt;
-
-#ifdef INTERCEPT_DEBUG
-        printf("[%03d] t: %3.3lf\tdist: %3.3lf\tvrel: %3.3lf\n", step, t, dist, vrel);
-#endif
 
         if(zero(square(dist - fmax(0.0, target_distance))/square(threshold))) {
             // minimization finished
-#ifdef INTERCEPT_DEBUG
-            printf("[%03d] minimization finished\n", step);
-#endif
             t_end = fmax(t_end, t + min_dt);
             break;
         } else if(sgn < 0 && fabs(dist-target_distance) < threshold) {
             // below threshold, do minimization step
-#ifdef INTERCEPT_DEBUG
-            printf("[%03d] minimization step\n", step);
-#endif
             double next = t + (target_distance - dist) / vrel;
             dt = fmax(fmin(next - t, (t1 - t)/2.0), (t0-t)/2.0);
-            //break;
         } else if(sgn > 0 && prev_sgn < 0 &&
             (t-prev_time)*vmax + threshold > fabs(dist - target_distance)
             && (t-prev_time) > (t1-t1)/(max_steps - step)) {
             // closest approach found, move time backwards and adjust time step
-#ifdef INTERCEPT_DEBUG
-            printf("[%03d] sign change, t: %lf\tprev_time: %lf\tmin_dt: %lf\n", step, t, prev_time, min_dt);
-#endif
 
             t_end = fmax(t, t_end);
             min_dt = (t - prev_time) / 2;
@@ -431,14 +343,10 @@ double intercept_search(
             sgn = prev_sgn;
         } else if(t > t1) {
             // search exhausted
-#ifdef INTERCEPT_DEBUG
-            printf("[%03d] search exhausted t: %lf\n", step, t);
-#endif
             break;
         } else {
             // searching, skip ahead in time
             double deltas[] = {
-                -1.0, //XXX:
                 // distance at maximum velocity
                 fabs((dist - target_distance)-threshold) / vmax,
                 // distance at relative velocity + max acceleration (may be NaN)
@@ -446,33 +354,15 @@ double intercept_search(
                 (vrel + sqrt(vrel*vrel - 4.0*amax*(dist-target_distance))) / amax,
             };
 
-            (void)amax; // XXX:
-
             for(unsigned i = 0; i < sizeof(deltas)/sizeof(double); ++i)
                 if(isfinite(deltas[i]))
                     dt = fmax(dt, deltas[i]);
-
-#ifdef INTERCEPT_DEBUG
-            printf("[%03d] skip ahead %2.2lfx, t: %3.3lf\n", step, dt/min_dt, t);
-#endif
-
         }
-
-        //if(zero(dt)) // TODO: terminate when dt gets small enough
-            //break;
 
         t_end = fmax(t_end, t);
         prev_time = t; prev_sgn = sgn;
         t += dt;
-// XXX: enable this
-#if 0
-        for(int o = 0; o < 2; ++o)
-            if(!conic_parabolic(e[o]))
-                E[o] += anomaly_dEdM(e[o], E[o]) * n[o] * dt;
-#endif
     }
-    //
-    // if(both circular && coplanar && prograde) t_end = t1;
 
     intercept->position[0] = pos[0]; intercept->position[1] = pos[1];
     intercept->velocity[0] = vel[0]; intercept->velocity[1] = vel[1];
@@ -486,11 +376,7 @@ double intercept_search(
     intercept->E1 = E[0]; intercept->E2 = E[1];
     intercept->xxx1 = NAN; intercept->xxx2 = NAN;
 
-#ifdef INTERCEPT_DEBUG
-    fclose(file);
-#endif
-
-    return t_end; // XXX: return value?
+    return t_end;
 }
 
 int intercept_orbit(
@@ -526,9 +412,6 @@ int intercept_orbit(
                 max_steps, intercept);
             if(intercept->distance <= threshold)
                 num_intercepts += 1;
-
-            // XXX: run intercept_search in loop until return value >= t_end
-            break;
         }
     }
 
